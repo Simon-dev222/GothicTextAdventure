34419e549471ca895ead8892536e522994ac2c1f
Simon <bramkampsimon@yahoo.de>
2025-12-23 17:15:24 +0100

    second commit

---
+++ Abenteuer.py
@@ -0,0 +1,43 @@
+RED = '\033[91m'    # Red color added
+GREEN = '\033[92m'  # Green color added
+BLUE = '\033[94m'   # Blue color added
+RESET = '\033[0m'   # This resets the color back to default
+
+print(f"{GREEN}""Willkommen zu Gothic!")
+print("In diesem Textbasierten Abenteuer wirst du die Welt von Gothic mit all seinen Gefahren,")
+print("Geheimnissen und Mythen kennen lernen.Lehn dich zurück und tauche ein in die Minenkolonie der Insel Khorinis")
+
+print(f"{RED}""Das Königreich Myrtana, wiedervereint durch die Hand König Rhobars II.")
+print("In den langen Jahren seiner Herrschaft war es ihm gelungen, alle Widersacher des Reiches zu bezwingen ... Bis auf einen.")
+print("Orks.")
+print("Der Krieg mit den Orks forderte seinen Tribut ...")
+print("... und die Gefangenen des Reiches sollten ihn bezahlen.")
+print("Der König brauchte Schwerter für seine Armeen und jeder, der sich eines auch noch so geringen Verbrechens schuldig gemacht hatte, "
+      "wurde zur Arbeit in den Erzminen von Khorinis gezwungen.")
+print("Aus allen Teilen des Landes wurden Gefangene gebracht.")
+print("Um jede Flucht unmöglich zu machen, sandte der König die mächtigsten Magier des Reiches aus, eine magische Barriere um das gesamte Tal zu errichten.")
+print("ICH war einer der Magier.")
+print("Wir mussten all unsere Macht bündeln, um die Barriere zu erschaffen, aber etwas störte das gebrechliche Gefüge der Magie.")
+print("Wir waren nun selbst Gefangene der Barriere.")
+print("Mehr als eine Sekunde der Unachtsamkeit brauchten die Gefangenen nicht - als wir zur Festung kamen, war keiner der Wärter mehr am Leben.")
+print("Und obwohl die Aufsässigen nicht wagten uns anzugreifen, waren wir machtlos.")
+print("Khorinis, mit all seinen Minen, war nun in den Händen der Gefangenen.")
+print("Der König hatte keine Wahl.")
+print("Er musste verhandeln, er brauchte das Erz.")
+print("Aber seine einstigen Sklaven verlangten nun einen hohen Preis.")
+print("Monat für Monat lieferte der König den Gefangenen alles, was sie verlangten, Monat für Monat brachten sie dafür das Erz an den Rand der Barriere.")
+print("In all den Jahren wurden viele Versuche unternommen, die magische Barriere zu öffnen, aber keiner hatte Erfolg.")
+print("Bis heute!")
+print("Ein weiterer Verurteilter wurde zur Klippe gebracht. Er wusste noch nichts von seinem Schicksal, aber ER sollte alles ändern.")
+
+
+
+
+
+
+
+
+
+
+print(f"{BLUE}""Du stehst an einer Klippe. Vor dir erstreckt sich eine Kuppel von gigantischem Ausmaß.")
+print("Unter dir erblickst du einen kleinen See mit einer Forderanlage.")
